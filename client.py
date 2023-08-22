4ea94172cff5ce7ea55c94429a7075b87ba1f670
FIEF-Nohell <noel.hmnn@gmail.com>
2023-08-22 20:10:25 +0200

    client expanded

--- client.py
+++ client.py
@@ -1,17 +1,35 @@
-
+import socket
+import pyaudio
+from threading import Thread
 
 class Client:
     def __init__(self, host, port):
-        print("dummy")
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect((host, port))
+        self.format = pyaudio.paInt16
+        self.channels = 2
+        self.rate = 44100
+        self.chunk = 1024
+        self.audio = pyaudio.PyAudio()
 
     def send_audio(self):
-        print("dummy")
+        stream = self.audio.open(format=self.format, channels=self.channels, rate=self.rate, input=True, frames_per_buffer=self.chunk)
+        while True:
+            data = stream.read(self.chunk)
+            self.client.send(data)
 
     def receive_audio(self):
-        print("dummy")
+        stream = self.audio.open(format=self.format, channels=self.channels, rate=self.rate, output=True, frames_per_buffer=self.chunk)
+        while True:
+            data = self.client.recv(self.chunk)
+            stream.write(data)
 
     def run(self):
-        print("dummy")
+        send_thread = Thread(target=self.send_audio)
+        receive_thread = Thread(target=self.receive_audio)
+        send_thread.start()
+        receive_thread.start()
 
 if __name__ == "__main__":
-    print("dummy")
+    client = Client("127.0.0.1", 12345)
+    client.run()
