40c929fbdfbf1c773ec5ff583149cf4efafbecaf
FIEF-Nohell <noel.hmnn@gmail.com>
2023-08-22 20:09:59 +0200

    init

---
+++ client.py
@@ -0,0 +1,17 @@
+
+
+class Client:
+    def __init__(self, host, port):
+        print("dummy")
+
+    def send_audio(self):
+        print("dummy")
+
+    def receive_audio(self):
+        print("dummy")
+
+    def run(self):
+        print("dummy")
+
+if __name__ == "__main__":
+    print("dummy")
