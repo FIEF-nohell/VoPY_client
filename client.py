93435ac86d64715fe9181ba87d665464456f4080
FIEF-Nohell <noel.hmnn@gmail.com>
2023-08-22 20:16:05 +0200

    added device list

--- client.py
+++ client.py
@@ -12,24 +12,34 @@ class Client:
         self.chunk = 1024
         self.audio = pyaudio.PyAudio()
 
-    def send_audio(self):
+    def getAudioDevices(self):
+        p = pyaudio.PyAudio()
+        info = p.get_host_api_info_by_index(0)
+        numdevices = info.get('deviceCount')
+        for i in range(0, numdevices):
+            if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+                print("Input Device ", i+1, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+        p.terminate()
+
+    def sendAudio(self):
         stream = self.audio.open(format=self.format, channels=self.channels, rate=self.rate, input=True, frames_per_buffer=self.chunk)
         while True:
             data = stream.read(self.chunk)
             self.client.send(data)
 
-    def receive_audio(self):
+    def receiveAudio(self):
         stream = self.audio.open(format=self.format, channels=self.channels, rate=self.rate, output=True, frames_per_buffer=self.chunk)
         while True:
             data = self.client.recv(self.chunk)
             stream.write(data)
 
     def run(self):
-        send_thread = Thread(target=self.send_audio)
-        receive_thread = Thread(target=self.receive_audio)
+        send_thread = Thread(target=self.sendAudio)
+        receive_thread = Thread(target=self.receiveAudio)
         send_thread.start()
         receive_thread.start()
 
 if __name__ == "__main__":
     client = Client("127.0.0.1", 12345)
-    client.run()
+    client.getAudioDevices()
+    ##client.run()
